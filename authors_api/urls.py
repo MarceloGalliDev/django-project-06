8bb86277cb1f5f7a2d05158133679efdaeca578d
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-12-23 17:33:40 -0300

    update
    update

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -3,7 +3,23 @@ URL configuration for authors_api project.
 """
 from django.contrib import admin  # type: ignore
 from django.urls import path  # type: ignore
-from django.conf import settings  # type: ignore
+from django.conf import settings, include  # type: ignore
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+get_schema_view = get_schema_view(
+    openapi.Info(
+        title="Authors Haven API",
+        default_version="v1",
+        description="API endpoint for Authors Haven API course",
+        contact=openapi.Contact(email="goat.tech@gmail.com"),
+        license=openapi.License(name="MIT License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny)
+)
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
--- core_apps/users/admin.py
+++ core_apps/users/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin
+# pylint: disable=all
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin  # type: ignore
+from django.utils.translation import gettext_lazy as _  # type: ignore
+from .forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+
+    list_display = ["pkid", "id", "email", "first_name", "last_name", "is_staff", "is_active"]
+
+    list_display_links = ["pkid", "id", "email"]
+
+    list_filter = ["email", "is_staff", "is_active"]
+
+    fieldsets = (
+        (_("Login credentials"), {"fields": ("email", "password")}),
+        (_("Persona info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions and Groups"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        None,
+        {
+            "classes": ("wide", ),
+            "fields": ("email", "first_name", "last_name", "password1", "password2"),
+        }
+    )
+
+    search_fields = ["email", "first_name", "last_name"]
+
+
+admin.site.register(User, UserAdmin)
